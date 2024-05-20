c8a91d24711bad989c98b418c7f1b81ee83d8cb6
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-21 00:31:51 +0200

    feat: Add Footer and SIteData

--- src/constants/site-data.ts
+++ src/constants/site-data.ts
@@ -1,22 +1,22 @@
 export const navItems = [
-  {
-    label: 'Sobre Nosotros',
-    href: '#sobre-nosotros'
-  },
-  {
-    label: 'Servicios',
-    href: '#servicios'
-  },
-  {
-    label: 'Casos de éxito',
-    href: '#casos-de-exito'
-  },
-  {
-    label: 'Testimonios',
-    href: '#testimonios'
-  },
-  {
-    label: 'Blog',
-    href: '/blog'
-  }
+	{
+		label: 'Sobre Nosotros',
+		href: '#sobre-nosotros',
+	},
+	{
+		label: 'Servicios',
+		href: '#servicios',
+	},
+	{
+		label: 'Casos de éxito',
+		href: '#casos-de-exito',
+	},
+	{
+		label: 'Testimonios',
+		href: '#testimonios',
+	},
+	{
+		label: 'Blog',
+		href: '/blog',
+	},
 ]

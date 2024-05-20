a8de5e0c6e34d15bf6bb275596de789d57aea661
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-21 00:19:29 +0200

    feat: Add Navbar

---
+++ src/constants/site-data.ts
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    label: 'Sobre Nosotros',
+    href: '#sobre-nosotros'
+  },
+  {
+    label: 'Servicios',
+    href: '#servicios'
+  },
+  {
+    label: 'Casos de éxito',
+    href: '#casos-de-exito'
+  },
+  {
+    label: 'Testimonios',
+    href: '#testimonios'
+  },
+  {
+    label: 'Blog',
+    href: '/blog'
+  }
+]

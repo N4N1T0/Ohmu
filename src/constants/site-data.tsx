d425e69f1313caf8538501bf8b8ce9370b14cfb6
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-06-21 18:19:37 +0200

    refactor: Add locale and change the data structure to reflect this

---
+++ src/constants/site-data.tsx
@@ -0,0 +1,92 @@
+import { DesarrolloWeb } from '@/assets/services'
+
+export const navItems = [
+	{
+		label: 'Sobre Nosotros',
+		href: '#sobre-nosotros',
+	},
+	{
+		label: 'Servicios',
+		href: '#servicios',
+	},
+	{
+		label: 'Casos de éxito',
+		href: '#casos-de-exito',
+	},
+	{
+		label: 'Testimonios',
+		href: '#testimonios',
+	},
+	{
+		label: 'Blog',
+		href: '/blog',
+	},
+]
+
+export const stats = {
+	es: [
+		{
+			label: 'Proyectos',
+			value: '30 +',
+		},
+		{
+			label: 'Clientes',
+			value: '50 +',
+		},
+		{
+			label: 'Satisfacción',
+			value: '95 %',
+		},
+		{
+			label: 'Experiencia',
+			value: '10 +',
+		},
+	],
+}
+
+export const services = {
+	es: [
+		{
+			title: 'Desarrollo Web',
+			description:
+				'Creamos sitios web modernos, escalables y seguros, adaptados a las necesidades específicas de tu negocio, garantizando una experiencia de usuario óptima y un rendimiento superior.',
+			image: DesarrolloWeb,
+		},
+		{
+			title: 'Marketing de Contenido',
+			description:
+				'Diseñamos y publicamos contenido estratégico y atractivo que refleja la identidad de tu marca, aumentando la visibilidad y el engagement con tu audiencia.',
+			image: DesarrolloWeb,
+		},
+		{
+			title: 'Gestión de Redes Sociales',
+			description:
+				'Optimizamos y gestionamos tus redes sociales con contenido relevante y campañas efectivas, fortaleciendo tu presencia online y fidelizando a tus seguidores.',
+			image: DesarrolloWeb,
+		},
+		{
+			title: 'Optimización de Retención de Clientes',
+			description:
+				'Implementamos estrategias avanzadas para mejorar la retención y lealtad de tus clientes, maximizando la rentabilidad y el valor a largo plazo de cada cliente.',
+			image: DesarrolloWeb,
+		},
+		{
+			title: 'Localización',
+			description:
+				'Aumentamos la visibilidad y rentabilidad de tus locales mediante estrategias de marketing local personalizadas, atrayendo más clientes a tu negocio físico.',
+			image: DesarrolloWeb,
+		},
+	],
+}
+
+export const uiInfo = {
+	es: {
+		'sub-hero': 'Transformamos tu presencia digital en una máquina de ventas',
+		'we-are-1':
+			'Somos una agencia boutique ágil y eficiente ubicada en Málaga, España,',
+		'we-are-2':
+			'Especializada en transformar tu negocio digital con soluciones innovadoras y personalizadas. Nuestro equipo de expertos utiliza las últimas tecnologías y enfoques basados en inteligencia artificial para desarrollar estrategias efectivas que impulsen tu crecimiento.',
+		'stats-1': 'nuestros logros',
+		'services-1': 'nuestros servicios',
+	},
+}

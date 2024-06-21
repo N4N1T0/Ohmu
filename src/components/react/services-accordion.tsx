ae5b7472d112e0a22b637c101eaa66c2d426472e
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-06-21 18:20:44 +0200

    refactor: Add basic structure for the services accordion

--- src/components/react/services-accordion.tsx
+++ src/components/react/services-accordion.tsx
@@ -6,22 +6,32 @@ import {
 } from '@/components/ui/accordion'
 import { services } from '@/constants/site-data'
 
-const ServicesAccordion = () => {
+const ServicesAccordion = ({ locale }: { locale: 'es' | 'en' | 'rs' }) => {
 	return (
 		<Accordion type='single' collapsible className='mt-10'>
-			{services.map(({ title, description }, index) => (
-				<AccordionItem value={title} key={title}>
-					<AccordionTrigger className='text-6xl uppercase flex py-10'>
-						<div className='flex items-center gap-4'>
-							<span className='text-3xl text-gold mr-5 md:mr-10'>
-								0{index + 1}
-							</span>
-							{title}
-						</div>
-					</AccordionTrigger>
-					<AccordionContent>{description}</AccordionContent>
-				</AccordionItem>
-			))}
+			{services[locale as keyof typeof services].map(
+				({ title, description, image }, index) => (
+					<AccordionItem value={title} key={title}>
+						<AccordionTrigger className='text-6xl uppercase flex py-10'>
+							<div className='flex items-center gap-4'>
+								<span className='text-3xl text-gold mr-5 md:mr-10'>
+									0{index + 1}
+								</span>
+								{title}
+							</div>
+						</AccordionTrigger>
+						<AccordionContent className='mx-24'>
+							<img
+								src={image.src}
+								alt={title}
+								title={`${title} Showcase`}
+								className='w-full rounded-[2rem] object-fit'
+							/>
+							<div>{description}</div>
+						</AccordionContent>
+					</AccordionItem>
+				),
+			)}
 		</Accordion>
 	)
 }

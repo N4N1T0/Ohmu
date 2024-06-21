4edee806692756d8d14e415b0ffe3efe936a2d9b
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-06-21 18:23:16 +0200

    refactor: minor changes

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,8 +1,10 @@
+import { LavandaDelLago1, LAvandaDelLago2 } from '@/assets/projects'
+
 export const projects = [
 	{
-		title: 'Ohmu Online Operations',
-		description: 'Software Development',
-		image: 'ohmu.png',
-		url: 'https://ohmu.online',
+		title: 'Lavanda del Lago',
+		description: 'Ecommerce de productos de Lavanda',
+		image1: LavandaDelLago1,
+		image2: LAvandaDelLago2,
 	},
 ]

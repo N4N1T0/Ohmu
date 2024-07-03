0f74e30c8bd6aa9e30bf5b0fc6d725378e81c23c
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-04 00:28:10 +0200

    refactor: Add all Locale Pages

---
+++ src/constants/ui-locale.ts
@@ -0,0 +1,49 @@
+export const uiInfo = {
+	es: {
+		'contact-sheet-button': 'Escribenos',
+		'contact-sheet-header': 'Nuestros metodos de contacto',
+		'contact-sheet-description': 'Escribinos para ponernos en contacto',
+		'contact-sheet-footer': 'Contactos directos',
+		'mobile-navbar-sheet-header': 'Ohmu Online Operations',
+		'sub-hero': 'Transformamos tu presencia digital en una máquina de ventas',
+		'we-are-1':
+			'Somos una agencia boutique ágil y eficiente ubicada en Málaga, España,',
+		'we-are-2':
+			'Especializada en transformar tu negocio digital con soluciones innovadoras y personalizadas. Nuestro equipo de expertos utiliza las últimas tecnologías y enfoques basados en inteligencia artificial para desarrollar estrategias efectivas que impulsen tu crecimiento.',
+		'stats-1': 'nuestros logros',
+		'services-moreIcon': 'ver más',
+		'services-1': 'nuestros servicios',
+		'project-header': 'Casos de exito',
+		'footer-title': 'Contactanos',
+		'footer-subtitle': 'Tienes alguna idea en mente',
+	},
+}
+
+export const uiForm = {
+	es: {
+		'contact-name': 'Nombre Completo',
+		'contact-name-placeholder': 'Jose Perez',
+		'contact-name-error': 'Por favor introduzca su nombre',
+		'contact-email': 'Email Personal',
+		'contact-email-placeholder': 'jose.perez@gmail.com',
+		'contact-email-error': 'Por favor introduzca su email',
+		'contact-phone': 'Numero de Telefono',
+		'contact-phone-placeholder': '+123456789',
+		'contact-phone-error': 'Por favor introduzca su telefono',
+		'contact-services': 'En que Podemos ayudarte',
+		'contact-services-placeholder': 'Convertir mi web en una maquina de ventas',
+		'contact-message': 'Un mensaje para nostros',
+		'contact-message-placeholder':
+			'Hola, que tal? queria que me ayudaran con...',
+		'contact-submit': 'Enviar',
+	},
+}
+
+export const projectUi = {
+	es: {
+		'project-goal': 'Objetivo',
+		'project-time': 'Tiempo',
+		'project-results': 'Resultados',
+		'project-links': ['Sitio', 'Codigo', 'Diseño'],
+	},
+}

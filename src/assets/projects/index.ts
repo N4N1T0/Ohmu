d724dfda27340f9820089a2e2f4dbfa6e8d58101
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-04 00:24:34 +0200

    feat: Add Projects Pages and Assets

--- src/assets/projects/index.ts
+++ src/assets/projects/index.ts
@@ -1,4 +1,15 @@
 import LavandaDelLago1 from './Snapinsta.app_387866164_18318160588109902_4880316353199509092_n_1024.webp'
 import LAvandaDelLago2 from './355shots_so.webp'
+import Backswing1 from './213shots_so.webp'
+import Backswing2 from './sven-mieke-T8FtYThew0w-unsplash.webp'
+import Backswing3 from './485shots_so.webp'
+import Backswing4 from './869shots_so.webp'
 
-export { LavandaDelLago1, LAvandaDelLago2 }
+export {
+	LavandaDelLago1,
+	LAvandaDelLago2,
+	Backswing1,
+	Backswing2,
+	Backswing3,
+	Backswing4,
+}
